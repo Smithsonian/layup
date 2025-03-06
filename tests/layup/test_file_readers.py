7b93a3b3a7abad8898bcf0b211e5b730de3812ae
drewoldag <47493171+drewoldag@users.noreply.github.com>
2025-03-06 14:30:24 -0800

    Add example data as .h5 files and add unit test ensure CSV and HDF5 readers are identical.

---
+++ tests/layup/test_file_readers.py
@@ -0,0 +1,32 @@
+import pytest
+from numpy.testing import assert_equal
+
+from layup.utilities.file_readers.CSVReader import CSVDataReader
+from layup.utilities.file_readers.HDF5Reader import HDF5DataReader
+from layup.utilities.data_utilities_for_tests import get_test_filepath
+
+
+@pytest.mark.parametrize("orbit_format", ["BCOM", "CART", "KEP"])
+def test_file_readers_produce_identical_values(orbit_format):
+    """Test that the HDF5 and CSV readers produce identical values."""
+
+    hdf5_reader = HDF5DataReader(get_test_filepath(f"{orbit_format}.h5"))
+    hdf5_data = hdf5_reader.read_rows()
+
+    csv_reader = CSVDataReader(get_test_filepath(f"{orbit_format}.csv"))
+    csv_data = csv_reader.read_rows()
+
+    assert_equal(hdf5_data, csv_data)
+
+
+@pytest.mark.parametrize("orbit_format", ["BCOM", "CART", "KEP"])
+def test_file_readers_produce_identical_values_with_cache(orbit_format):
+    """Test that the HDF5 and CSV readers produce identical values when using the cache"""
+
+    hdf5_reader = HDF5DataReader(get_test_filepath(f"{orbit_format}.h5"), cache_table=True)
+    hdf5_data = hdf5_reader.read_rows()
+
+    csv_reader = CSVDataReader(get_test_filepath(f"{orbit_format}.csv"), cache_table=True)
+    csv_data = csv_reader.read_rows()
+
+    assert_equal(hdf5_data, csv_data)
