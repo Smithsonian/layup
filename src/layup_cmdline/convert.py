df326db6ab0159a57c8cbdfc286992cda32d3458
Ryan Lyttle <rlyttle09@qub.ac.uk>
2025-03-10 12:37:34 -0700

    adding cmd_line arguments to layup convert

--- src/layup_cmdline/convert.py
+++ src/layup_cmdline/convert.py
@@ -11,8 +11,51 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="This would convert orbits",
     )
+    required = parser.add_argument_group("Required arguments")
 
+    required.add_argument(
+        "-i",
+        "--input",
+        help= "input orbit file",
+        dest="i",
+        type=str,
+        required=True,
+    )
+    required.add_argument(
+        "-f",
+        "--format",
+        help= "format of input file",
+        dest="f",
+        type=str,
+        required=True,
+    )
+    required.add_argument(
+        "-t",
+        "--type",
+        help="orbit type to convert to",
+        dest="t",
+        type=str,
+        required=True,
+    )
+    required.add_argument(
+        "-o",
+        "--output",
+        help= "output file name. default path is current working directory",
+        dest='o',
+        type=str,
+        required=True,
+    )
     optional = parser.add_argument_group("Optional arguments")
+    optional.add_argument(
+        "-c",
+        "--chunksize",
+        help="number of orbits to be processed at once",
+        dest="c",
+        type=int,
+        default= 10000,
+        required=False,
+    )
+    
     optional.add_argument(
         "-p",
         "--print",
