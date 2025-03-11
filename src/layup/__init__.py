7c65749ba7715ab72a8544ee6aa0439be293b695
Max West <maxwest@uw.edu>
2025-03-11 12:23:45 -0700

    add it to the routines __init__

--- src/layup/__init__.py
+++ src/layup/__init__.py
@@ -9,12 +9,3 @@ if not os.path.isfile("./libassist.so"):
 
 if not os.path.isfile("./librebound.so"):
     os.symlink("./include/rebound/src/librebound.so", "./librebound.so")
-
-# make libraries discoverable for linux
-if platform == "linux" or platform == "linux2":
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    if "LD_LIBRARY_PATH" in os.environ.keys():
-        ld_lib_path = os.environ["LD_LIBRARY_PATH"]
-        os.environ["LD_LIBRARY_PATH"] = ld_lib_path + ":" + root_dir
-    else:
-        os.environ["LD_LIBRARY_PATH"] = root_dir
