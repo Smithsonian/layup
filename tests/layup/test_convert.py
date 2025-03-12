2f1c16e8b7b4f9d1f13fe33331f8b64b584b9b8f
Wilson Beebe <wbeebe@uw.edu>
2025-03-12 11:01:21 -0700

    Update test_convert to use pytest fixture for the temp directory

--- tests/layup/test_convert.py
+++ tests/layup/test_convert.py
@@ -6,7 +6,6 @@ from layup.utilities.file_io.HDF5Reader import HDF5DataReader
 
 from numpy.testing import assert_equal
 import os
-import tempfile
 import pytest
 
 
@@ -31,31 +30,30 @@ import pytest
         (1, -1),
     ],
 )
-def test_convert_round_trip_csv(chunk_size, num_workers):
+def test_convert_round_trip_csv(tmpdir, chunk_size, num_workers):
     """Test that the convert function works for a small CSV file."""
     input_csv_reader = CSVDataReader(get_test_filepath("CART.csv"), "csv")
     input_data = input_csv_reader.read_rows()
 
-    # Create a temporary file to write the output to
-    with tempfile.TemporaryDirectory() as temp_out_dir:
-        output_file_stem = "test_output"
-        os.chdir(temp_out_dir)
-        temp_out_file = os.path.join(temp_out_dir, f"{output_file_stem}.csv")
-        convert_cli(
-            get_test_filepath("CART.csv"),
-            output_file_stem,
-            "BCOM",
-            "csv",
-            chunk_size=chunk_size,
-            num_workers=num_workers,
-        )
+    # Since the convert CLI outputs to the current working directory, we need to change to our temp directory
+    output_file_stem = "test_output"
+    os.chdir(tmpdir)
+    temp_out_file = os.path.join(tmpdir, f"{output_file_stem}.csv")
+    convert_cli(
+        get_test_filepath("CART.csv"),
+        output_file_stem,
+        "BCOM",
+        "csv",
+        chunk_size=chunk_size,
+        num_workers=num_workers,
+    )
 
-        assert os.path.exists(temp_out_file)
+    assert os.path.exists(temp_out_file)
 
-        output_csv_reader = CSVDataReader(temp_out_file, "csv")
-        output_data = output_csv_reader.read_rows()
-        # TODO we can round trip to test but right now convert simply copies and reappends data
-        assert_equal(input_data, output_data)
+    output_csv_reader = CSVDataReader(temp_out_file, "csv")
+    output_data = output_csv_reader.read_rows()
+    # TODO we can round trip to test but right now convert simply copies and reappends data
+    assert_equal(input_data, output_data)
 
 
 @pytest.mark.parametrize(
@@ -79,28 +77,27 @@ def test_convert_round_trip_csv(chunk_size, num_workers):
         (1, -1),
     ],
 )
-def test_convert_one_chunk_one_worker_hdf5(chunk_size, num_workers):
+def test_convert_one_chunk_one_worker_hdf5(tmpdir, chunk_size, num_workers):
     """Test that the convert function works for a small HDF5 file."""
     input_hdf5_reader = HDF5DataReader(get_test_filepath("CART.h5"))
     input_data = input_hdf5_reader.read_rows()
 
-    # Create a temporary file to write the output to
-    with tempfile.TemporaryDirectory() as temp_out_dir:
-        output_file_stem = "test_output"
-        os.chdir(temp_out_dir)
-        temp_out_file = os.path.join(temp_out_dir, f"{output_file_stem}.h5")
-        convert_cli(
-            get_test_filepath("CART.h5"),
-            temp_out_file,
-            "BCOM",
-            "hdf5",
-            chunk_size=chunk_size,
-            num_workers=num_workers,
-        )
+    # Since the convert CLI outputs to the current working directory, we need to change to our temp directory
+    output_file_stem = "test_output"
+    os.chdir(tmpdir)
+    temp_out_file = os.path.join(tmpdir, f"{output_file_stem}.h5")
+    convert_cli(
+        get_test_filepath("CART.h5"),
+        temp_out_file,
+        "BCOM",
+        "hdf5",
+        chunk_size=chunk_size,
+        num_workers=num_workers,
+    )
 
-        assert os.path.exists(temp_out_file)
+    assert os.path.exists(temp_out_file)
 
-        output_hdf5_reader = HDF5DataReader(temp_out_file)
-        output_data = output_hdf5_reader.read_rows()
-        # TODO we can round trip to test but right now convert simply copies and reappends data
-        assert_equal(input_data, output_data)
+    output_hdf5_reader = HDF5DataReader(temp_out_file)
+    output_data = output_hdf5_reader.read_rows()
+    # TODO we can round trip to test but right now convert simply copies and reappends data
+    assert_equal(input_data, output_data)
