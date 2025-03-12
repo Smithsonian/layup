cd0b22d81a952067ea37504acc1a4b15f96ff34d
Max West <maxwest@uw.edu>
2025-03-12 11:59:41 -0700

    create symlinks at build time?

---
+++ create_lib_links.sh
@@ -0,0 +1,7 @@
+current_dir=$(pwd)
+
+ln -sf "$current_dir/include/assist/src/libassist.so" "$current_dir/libassist.so"
+ln -sf "$current_dir/include/assist/src/libassist.so" "$current_dir/src/libassist.so"
+
+ln -sf "$current_dir/include/assist/src/librebound.so" "$current_dir/librebound.so"
+ln -sf "$current_dir/include/assist/src/librebound.so" "$current_dir/src/librebound.so"
--- src/layup/__init__.py
+++ src/layup/__init__.py
@@ -6,22 +6,22 @@ from pathlib import Path
 # in the main directory, ensuring that they are discoverable
 # to cpython at runtime.
 
-root_dir = Path(__file__).parent.parent.parent
-assist_lib_path = os.path.join(root_dir, "include/assist/src/libassist.so")
-assist_sym_path_linux = os.path.join(root_dir, "src/libassist.so")
-assist_sym_path_osx = os.path.join(root_dir, "libassist.so")
-if not os.path.isfile(assist_sym_path_linux):
-    os.symlink(assist_lib_path, assist_sym_path_linux)
-if not os.path.isfile(assist_sym_path_osx):
-    os.symlink(assist_lib_path, assist_sym_path_osx)
+# root_dir = Path(__file__).parent.parent.parent
+# assist_lib_path = os.path.join(root_dir, "include/assist/src/libassist.so")
+# assist_sym_path_linux = os.path.join(root_dir, "src/libassist.so")
+# assist_sym_path_osx = os.path.join(root_dir, "libassist.so")
+# if not os.path.isfile(assist_sym_path_linux):
+#     os.symlink(assist_lib_path, assist_sym_path_linux)
+# if not os.path.isfile(assist_sym_path_osx):
+#     os.symlink(assist_lib_path, assist_sym_path_osx)
 
-rebound_lib_path = os.path.join(root_dir, "include/rebound/src/librebound.so")
-rebound_sym_path_linux = os.path.join(root_dir, "src/librebound.so")
-rebound_sym_path_osx = os.path.join(root_dir, "librebound.so")
-if not os.path.isfile(rebound_sym_path_linux):
-    os.symlink(rebound_lib_path, rebound_sym_path_linux)
-if not os.path.isfile(rebound_sym_path_osx):
-    os.symlink(rebound_lib_path, rebound_sym_path_osx)
+# rebound_lib_path = os.path.join(root_dir, "include/rebound/src/librebound.so")
+# rebound_sym_path_linux = os.path.join(root_dir, "src/librebound.so")
+# rebound_sym_patgh_osx = os.path.join(root_dir, "librebound.so")
+# if not os.path.isfile(rebound_sym_path_linux):
+#     os.symlink(rebound_lib_path, rebound_sym_path_linux)
+# if not os.path.isfile(rebound_sym_path_osx):
+#     os.symlink(rebound_lib_path, rebound_sym_path_osx)
 try:
     from ._version import version as __version__
     from ._version import version_tuple
