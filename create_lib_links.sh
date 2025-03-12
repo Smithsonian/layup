fd159a3230aa94ea21e42cd93592d9e8da83ef57
Max West <maxwest@uw.edu>
2025-03-12 14:20:44 -0700

    temporary workaround for LD_LIBRARY_PATH

--- create_lib_links.sh
+++ create_lib_links.sh
@@ -3,5 +3,5 @@ current_dir=$(pwd)
 ln -sf "$current_dir/include/assist/src/libassist.so" "$current_dir/libassist.so"
 ln -sf "$current_dir/include/assist/src/libassist.so" "$current_dir/src/libassist.so"
 
-ln -sf "$current_dir/include/assist/src/librebound.so" "$current_dir/librebound.so"
-ln -sf "$current_dir/include/assist/src/librebound.so" "$current_dir/src/librebound.so"
+ln -sf "$current_dir/include/rebound/src/librebound.so" "$current_dir/librebound.so"
+ln -sf "$current_dir/include/rebound/src/librebound.so" "$current_dir/src/librebound.so"
--- src/layup/__init__.py
+++ src/layup/__init__.py
@@ -1,27 +1,3 @@
-import os
-from sys import platform
-from pathlib import Path
-
-# create symlinks for the assist and rebound shared objects
-# in the main directory, ensuring that they are discoverable
-# to cpython at runtime.
-
-# root_dir = Path(__file__).parent.parent.parent
-# assist_lib_path = os.path.join(root_dir, "include/assist/src/libassist.so")
-# assist_sym_path_linux = os.path.join(root_dir, "src/libassist.so")
-# assist_sym_path_osx = os.path.join(root_dir, "libassist.so")
-# if not os.path.isfile(assist_sym_path_linux):
-#     os.symlink(assist_lib_path, assist_sym_path_linux)
-# if not os.path.isfile(assist_sym_path_osx):
-#     os.symlink(assist_lib_path, assist_sym_path_osx)
-
-# rebound_lib_path = os.path.join(root_dir, "include/rebound/src/librebound.so")
-# rebound_sym_path_linux = os.path.join(root_dir, "src/librebound.so")
-# rebound_sym_patgh_osx = os.path.join(root_dir, "librebound.so")
-# if not os.path.isfile(rebound_sym_path_linux):
-#     os.symlink(rebound_lib_path, rebound_sym_path_linux)
-# if not os.path.isfile(rebound_sym_path_osx):
-#     os.symlink(rebound_lib_path, rebound_sym_path_osx)
 try:
     from ._version import version as __version__
     from ._version import version_tuple
