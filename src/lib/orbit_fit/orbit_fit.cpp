55fd7a5f3cd2111254887163fa431e668d53a1ea
Max West <110124344+maxwest-uw@users.noreply.github.com>
2025-03-13 10:31:34 -0700

    includes c and c++ dependencies and configure compilation (#67)
    * includes c and c++ dependencies and configure compilation.
    
    * ensure github actions have submodules
    
    * maybe make .so files discoverable on linux
    
    * maybe a syntax problem?
    
    * add it to the routines __init__
    
    * add symlinks everywhere
    
    * create symlinks at build time?
    
    * temporary workaround for LD_LIBRARY_PATH
    
    * jfc just comment out the gauss code for now to see if the build stuff works
    
    * fix readthedocs?
    
    * get gauss code compiling properly
    
    * add linux instructions to the README
    
    * whoops

--- src/lib/orbit_fit/orbit_fit.cpp
+++ src/lib/orbit_fit/orbit_fit.cpp
@@ -27,7 +27,7 @@
 #include <complex>
 
 #include "orbit_fit.h"
-#include "gauss.h"
+#include "../gauss/gauss.cpp"
 
 extern "C"{
 #include "rebound.h"
@@ -499,8 +499,8 @@ void create_sequences(std::vector<double>& times,
 
 int orbit_fit(struct assist_ephem* ephem,
 	      struct reb_particle& p0, double epoch,
-	      std::vector<double>& times,	      
-	      std::vector<detection>& detections,
+	      std::vector<double>& times,	      // not modified
+	      std::vector<detection>& detections, // not modified
 	      std::vector<residuals>& resid_vec,
 	      std::vector<partials>& partials_vec,
 	      size_t& iters,
