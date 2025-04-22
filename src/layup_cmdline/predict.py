20180cf9794e7d3c6ce712fae820e000454b94da
Meg Schwamb <megan.schwamb@gmail.com>
2025-04-22 13:57:39 +0100

    added some additional command line options
    added in some more command line options for layup predict that were in convert and not in the predict verb. Added a check if the user sets the number of days to less than 0.

--- src/layup_cmdline/predict.py
+++ src/layup_cmdline/predict.py
@@ -19,6 +19,7 @@ def main():
     )
 
     positionals = parser.add_argument_group("Positional arguments")
+
     positionals.add_argument(
         help="input orbit file",
         dest="input",
@@ -26,6 +27,89 @@ def main():
     )
 
     optional = parser.add_argument_group("Optional arguments")
+
+    optional.add_argument(
+        "--ar",
+        "--ar-data-path",
+        help="Directory path where Assist+Rebound data files where stored when running bootstrap_sorcha_data_files from the command line.",
+        type=str,
+        dest="ar_data_file_path",
+        required=False,
+    )
+    optional.add_argument(
+        "-c",
+        "--conf",
+        help="optional configuration file",
+        type=str,
+        dest="c",
+        required=False,
+    )
+    optional.add_argument(
+        "-ch",
+        "--chunksize",
+        help="number of orbits to be processed at once",
+        dest="chunk",
+        type=int,
+        default=10000,
+        required=False,
+    )
+
+    optional.add_argument(
+        "-d",
+        "--days",
+        help="number of days to predict on sky positions. Only used if the end date is not specified on the command line",
+        dest="days",
+        type=int,
+        default=14,
+        required=False,
+    )
+    optional.add_argument(
+        "-e",
+        "--end-date",
+        help="end date, mjd. Only specified if not specifying the number of days",
+        dest="e",
+        type=str,
+        default=None,
+        required=False,
+    )
+
+    optional.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Overwrite output file",
+    )
+
+    optional.add_argument(
+        "-i",
+        "--input-type",
+        help="input format type of file (csv or hdf5)",
+        dest="i",
+        type=str,
+        default="csv",
+        required=False,
+    )
+
+    optional.add_argument(
+        "-n",
+        "--num-workers",
+        help="Number of CPU workers to use for parallel processing each chunk. -1 uses all available CPUs.",
+        dest="n",
+        type=int,
+        default=-1,
+        required=False,
+    )
+
+    optional.add_argument(
+        "-o",
+        "--output",
+        help="output file stem. default path is current working directory",
+        dest="o",
+        type=str,
+        default="predicted_output",
+        required=False,
+    )
+
     optional.add_argument(
         "-s",
         "--start-date",
@@ -37,48 +121,17 @@ def main():
         ),  # gives current time in mjd_UTC
         required=False,
     )
-    optional.add_argument(
-        "-n",
-        "--num-nights",
-        help="number of nights to predict on sky positions",
-        dest="n",
-        type=int,
-        default=14,
-        required=False,
-    )
-    optional.add_argument(
-        "-e",
-        "--end-date",
-        help="end date, mjd",
-        dest="e",
-        type=str,
-        default=None,
-        required=False,
-    )
-    optional.add_argument(
-        "-o",
-        "--output",
-        help="output file stem. default path is current working directory",
-        dest="o",
-        type=str,
-        default="predicted_output",
-        required=False,
-    )
+
     optional.add_argument(
         "-t",
         "--timestep",
-        help="timestep for predict. must be string consisting of float and unit [min,day,sec,h] with no spaces",
+        help="timestep for predict. must be string consisting of float followed by the unit (d for day, h for hour, m for minutes, s for seconds) e.g. 1h or 30m",
         dest="t",
         type=str,
-        default="1min",  # we would want to parse the float and the unit, and convert into day unit
+        default="1h",  # we would want to parse the float and the unit, and convert into day unit
         required=False,
     )
-    optional.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Overwrite output file",
-    )
+
     args = parser.parse_args()
 
     return execute(args)
@@ -99,6 +152,11 @@ def execute(args):
 
     # check for overwriting output file
     warn_or_remove_file(str(args.o), args.force, logger)
+
+    # check that days is greater than zero if being used
+
+    if (args.e == None) and args.days <= 0:
+        sys.exit(f"Number of days must be greater than 0. Value entered was : {args.days}")
 
     # converting timestep argument args.t into a float in day units.
     timestep_str = args.t
