96fa0d35c81517ee52dc3bd224cbd21d973da23b
Ryan Lyttle <rlyttle09@qub.ac.uk>
2025-04-28 11:18:16 -0700

    created new verb demo

---
+++ src/layup/utilities/layup_demo_commands.py
@@ -0,0 +1,42 @@
+
+
+def print_demo_command(verb,printall=True):
+    """
+    Prints the current working version of the layup orbitfit demo command to the terminal, with
+    optional functionality to also tell the user how to copy the demo files.
+
+    Parameters
+    -----------
+    printall : boolean
+        When True, prints the demo command plus the instructions for copying the demo files.
+        When False, prints the demo command only.
+
+    Returns
+    -----------
+    None.
+
+    """
+    if verb == "orbitfit":
+
+        current_demo_command = "layup orbitfit cmd (to be added)"
+
+    print("\nThe command to run the layup orbitfit demo in this version of layup is:\n")
+
+    print("    \033[1;38;2;255;165;0m" + current_demo_command+ "\033[0m\n")
+
+
+    print("WARNING: This command assumes that the demo files are in your working directory.\n")
+
+    
+    if printall:
+        print("You can copy the demo files into your working directory by running:\n")
+
+        print("    \033[1;38;2;255;165;0mlayup demo prepare -v "+verb+"\033[0m\n")
+
+        print("Or, to copy them into a directory of your choice, run:\n")
+
+        print("    \033[1;38;2;255;165;0mlayup demo prepare -v "+verb+" -p /path/to/files \033[0m\n")
+
+    print(
+        "If copying into a directory of your choice, you will need to modify the demo command to path to your files.\n"
+    )
---
+++ src/layup_cmdline/demo.py
@@ -0,0 +1,130 @@
+from layup_cmdline.layupargumentparser import LayupArgumentParser
+
+#
+# layup demo prepare
+#
+
+
+def cmd_demo_prepare(args):  # pragma: no cover
+    #
+    # NOTE: DO NOT MOVE THESE IMPORTS TO THE TOP LEVEL OF THE MODULE !!!
+    #
+    #       Importing layup from the function and not at the top-level of the module
+    #       allows us to exit quickly and print the help/error message (in case there
+    #       was a mistake on the command line). Importing layup can take 5 seconds or
+    #       more, and making the user wait that long just to print out an erro message
+    #       is poor user experience.
+    #
+
+
+    import sys
+    
+    if args.verb not in ['comet', 'convert', 'demo', 'orbitfit', 'predict', 'visualize']:
+        sys.exit("not a layup verb try ['comet', 'convert', 'demo', 'orbitfit', 'predict', 'visualize']")
+    if args.verb == "orbitfit":
+        print("creating demo files")
+
+
+
+        # from layup.utilities.layup_copy_demo_files import copy_demo_files
+        # import os
+
+        # copy_location = os.path.abspath(args.path)
+        # copy_demo_files(copy_location, args.force)
+
+    else: 
+        print(f'Demo not yet created for {args.verb}')
+    
+    
+    
+
+#
+# layup demo howto
+#
+def cmd_demo_howto(args):  # pragma: no cover
+    #
+    # NOTE: DO NOT MOVE THESE IMPORTS TO THE TOP LEVEL OF THE MODULE !!!
+    #
+    #       Importing layup from the function and not at the top-level of the module
+    #       allows us to exit quickly and print the help/error message (in case there
+    #       was a mistake on the command line). Importing layup can take 5 seconds or
+    #       more, and making the user wait that long just to print out an erro message
+    #       is poor user experience.
+    #
+    import sys
+    
+    if args.verb not in ['comet', 'convert', 'demo', 'orbitfit', 'predict', 'visualize']:
+        sys.exit("not a layup verb try ['comet', 'convert', 'demo', 'orbitfit', 'predict', 'visualize']")
+    if args.verb == "orbitfit":
+        from layup.utilities.layup_demo_commands import print_demo_command
+
+        return print_demo_command(args.verb)
+    else: 
+        print(f'Demo not yet created for {args.verb}')
+
+#
+# layup demo
+#
+
+
+def main():
+    # Create the top-level parser
+    parser = LayupArgumentParser(
+        prog="layup demo", description="Prepare and explain how to run layup demos"
+    )
+    subparsers = parser.add_subparsers(
+        title="commands",
+        description="Available commands",
+        help="Command to execute must also specify verb with -v --verb",
+        dest="command",
+    )
+
+    # Add the `prepare` subcommand
+    demo_prepare_parser = subparsers.add_parser(
+        "prepare", help="Prepares the files necessary to run the demo"
+    )
+    demo_prepare_parser.set_defaults(func=cmd_demo_prepare)
+    demo_prepare_parser.add_argument(
+        "-v",
+        "--verb",
+        type=str,
+        required = True,
+        help="which verb to prepare from ['comet', 'convert', 'demo','orbitfit', 'predict', 'visualize']",
+    )
+    demo_prepare_parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        default="./",
+        help="Filepath where you want to copy the demo files. Default is current working directory.",
+    )
+    demo_prepare_parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        default=False,
+        help="Force deletion/overwrite of existing demo file(s). Default False.",
+    )
+
+    # Add the `howto` subcommand
+    demo_howto_parser = subparsers.add_parser("howto", help="Show the command to run the layup demo")
+    demo_howto_parser.set_defaults(func=cmd_demo_howto)
+    demo_howto_parser.add_argument(
+        "-v",
+        "--verb",
+        type=str,
+        required = True,
+        help="which verb to prepare from ['comet', 'convert', 'demo', 'orbitfit', 'predict', 'visualize']",
+    )
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the appropriate function based on the subcommand
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
