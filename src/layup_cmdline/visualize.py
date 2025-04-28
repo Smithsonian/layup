61f9337588be70647485dc5415145e576d1b76ab
Ryan Lyttle <rlyttle09@qub.ac.uk>
2025-04-28 14:38:33 -0700

    added cmd line args and checks to visualize verb

--- src/layup_cmdline/visualize.py
+++ src/layup_cmdline/visualize.py
@@ -3,6 +3,10 @@
 #
 import argparse
 from layup_cmdline.layupargumentparser import LayupArgumentParser
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -39,6 +43,24 @@ def main():
         required=False,
     )
     optional.add_argument(
+        "-d",
+        "--dimensions",
+        help="dimensions the plot will be in [2D, 3D]",
+        dest="d",
+        type=str,
+        default="2D",
+        required=False,
+    )
+    optional.add_argument(
+        "-b",
+        "--backend",
+        help="backend used for plotting [matplot, plotly]",
+        dest="b",
+        type=str,
+        default="matplot",
+        required=False,
+    )
+    optional.add_argument(
         "-o",
         "--output",
         help="output file stem. default path is current working directory",
@@ -54,7 +76,37 @@ def main():
 
 
 def execute(args):
-    print("Hello world this would start visualise")
+
+    from layup.utilities.file_access_utils import find_file_or_exit, find_directory_or_exit
+
+    # from layup.visualize import visualize_cli
+
+    # check input exists
+    find_file_or_exit(args.input, "input")
+
+    # Check that output directory exists
+    find_directory_or_exit(args.o, "-o, --")
+
+    # check format of input file
+    if args.i.lower() == "csv":
+        output_file = args.o + ".csv"
+    elif args.i.lower() == "hdf5":
+        output_file = args.o + ".h5"
+    else:
+        sys.exit("ERROR: File format must be 'csv' or 'hdf5'")
+
+    if args.d.upper() not in ["2D", "3D"]:
+        sys.exit("ERROR: -d --dimensions must be '2D' or '3D'")
+
+    if args.b not in ["matplot", "plotly"]:
+        sys.exit("ERROR: -b --backend must be 'matplot' or 'plotly'")
+    # visualize_cli(
+    #     input = args.input,
+    #     num = args.n,
+    #     dimensions = args.d,
+    #     backend = args.b,
+    #     output = output_file,
+    # )
 
 
 if __name__ == "__main__":
