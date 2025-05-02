ee962fad6fab2a13bb5a8c7d69efdc8be38c4c6b
Max West <maxwest@uw.edu>
2025-05-02 14:38:51 -0700

    fully running cli->python->c++->output

--- src/layup/predict.py
+++ src/layup/predict.py
@@ -4,13 +4,33 @@ from pathlib import Path
 from layup.utilities.data_processing_utilities import process_data
 from layup.routines import predict_sequence, Observation, FitResult, get_ephem, numpy_to_eigen
 from layup.utilities.file_io import CSVDataReader
-from layup.utilities.data_processing_utilities import LayupObservatory, parse_fit_result
+from layup.utilities.data_processing_utilities import LayupObservatory, parse_fit_result, create_chunks
 from layup.utilities.datetime_conversions import convert_tdb_date_to_julian_date
+from layup.utilities.file_io.file_output import write_csv
 import spiceypy as spice
 import numpy as np
 import pooch
 
-def _predict(data, obs_pos_vel, times, cache_dir):
+
+def _get_result_dtypes(primary_id_column_name: str):
+    """Helper function to create the result dtype with the correct primary ID column name."""
+    # Define a structured dtype to match the OrbfitResult fields
+    return np.dtype(
+        [
+            (primary_id_column_name, "O"),  # Object ID
+            ("rho_x", "f8"),  # The first of the 3 rho unit vector
+            ("rho_y", "f8"),
+            ("rho_z", "f8"),
+            ("obs_cov0", "f8"),  # The first of 4 of the observer covariance
+            ("obs_cov1", "f8"),
+            ("obs_cov2", "f8"),
+            ("obs_cov3", "f8"),
+            ("epoch", "f8"),  # Time for prediction
+        ]
+    )
+
+
+def _predict(data, obs_pos_vel, times, cache_dir, primary_id_column_name):
     """This function is called by the parallelization function to call the C++ code."""
     if cache_dir is None:
         kernels_loc = str(pooch.os_cache("layup"))
@@ -36,23 +56,24 @@ def _predict(data, obs_pos_vel, times, cache_dir):
             cov.append(row[f"cov_{i}"])
         cov = np.array(cov)
 
-        predict_results.append(
-            predict_sequence(
-                get_ephem(kernels_loc),
-                fit,
-                observations,
-                numpy_to_eigen(cov, 6, 6)
+        pred_res = predict_sequence(get_ephem(kernels_loc), fit, observations, numpy_to_eigen(cov, 6, 6))
+
+        for pred in pred_res:
+            predict_results.append(
+                (
+                    row[primary_id_column_name],
+                    pred.rho[0],
+                    pred.rho[1],
+                    pred.rho[2],
+                    pred.obs_cov[0],
+                    pred.obs_cov[1],
+                    pred.obs_cov[2],
+                    pred.obs_cov[3],
+                    pred.epoch,
+                )
             )
-        )
 
-    for p in predict_results:
-        for r in p:
-            print(r)
-            print(r.rho)
-            print(r.obs_cov)
-            print(r.epoch)
-
-    return np.array([1])
+    return np.array(predict_results, dtype=_get_result_dtypes(primary_id_column_name))
 
 
 def predict(data, obscode, times, primary_id_column_name="provID", num_workers=-1, cache_dir=None):
@@ -62,22 +83,20 @@ def predict(data, obscode, times, primary_id_column_name="provID", num_workers=-
 
     Layup_observatory = LayupObservatory()
 
-    times_et = np.array([
-        spice.str2et(f"jd {t} tdb") for t in times
-    ], dtype="<f8")
+    times_et = np.array([spice.str2et(f"jd {t} tdb") for t in times], dtype="<f8")
 
-    obs_data  = np.array([
-        (obscode, t) for t in times_et
-    ], dtype=[("stn", "<U10"), ("et", "<f8")])
+    obs_data = np.array([(obscode, t) for t in times_et], dtype=[("stn", "<U10"), ("et", "<f8")])
 
     obs_pos_vel = Layup_observatory.obscodes_to_barycentric(obs_data)
 
-    return process_data(data,
+    return process_data(
+        data,
         n_workers=num_workers,
         func=_predict,
         obs_pos_vel=obs_pos_vel,
         times=times,
         cache_dir=cache_dir,
+        primary_id_column_name=primary_id_column_name,
     )
 
 
@@ -99,18 +118,11 @@ def predict_cli(
     if num_workers < 0:
         num_workers = os.cpu_count()
 
-    times = np.arange(
-        start_date,
-        end_date+1
-    )
+    times = np.arange(start_date, end_date + 1, step=timestep_day)
 
-    reader = CSVDataReader(
-        input_file,
-        primary_id_column_name=_primary_id_column_name,
-        sep="csv"
-    )
+    reader = CSVDataReader(input_file, primary_id_column_name=_primary_id_column_name, sep="csv")
 
-    chunks = _create_chunks(reader, chunk_size=cli_args.chunk)
+    chunks = create_chunks(reader, chunk_size=cli_args.chunk)
 
     for chunk in chunks:
         # Read the objects from the file
@@ -121,69 +133,10 @@ def predict_cli(
             obscode=cli_args.station,
             times=times,
             num_workers=cli_args.n,
-            cache_dir=cache_dir
+            cache_dir=cache_dir,
+            primary_id_column_name=cli_args.primary_id_column_name,
         )
 
-    # data = None
-
-    # results = predict(data, num_workers=num_workers)
-
-    # Write the results to the output file
+        if len(predictions) > 0:
+            write_csv(predictions, output_file)
     pass
-
-# TODO: abstract this away
-def _create_chunks(reader, chunk_size):
-    """For a given reader create a list of lists of object ids such that the total
-    number of entries in the file for all object ids in a given list, will be
-    less than the chunk size.
-
-    Parameters
-    ----------
-    reader : ObjectDataReader
-        The file reader object for the input file
-    chunk_size : int
-        The maximum number of rows to be included in a single list of ids
-
-    Returns
-    -------
-    chunks : list[list[ObjIds]]
-        A list of lists of object ids that can be passed to the reader's read_objects
-        method.
-    """
-    # Force the reader to build the id table and id count dictionary
-    reader._build_id_map()
-
-    # Find all object ids with more rows than the max allowed number of rows.
-    exceeds_id_list = []
-    for k, v in reader.obj_id_counts.items():
-        if v > chunk_size:
-            exceeds_id_list.append(k)
-
-    # Log an error if the any of the objects have more rows than the chunk size
-    if exceeds_id_list:
-        logger.error("The following objects have more rows than the max allowed number of rows.")
-        for k in exceeds_id_list:
-            logger.error(f"Object id {k} has {reader.obj_id_counts[k]} rows")
-        raise ValueError("At least one object has more rows than the max allowed number of rows.")
-
-    chunks = []
-    obj_ids_in_chunk = []
-    accumulator = 0
-
-    # Loop over the object id counts dictionary
-    for k, v in reader.obj_id_counts.items():
-        # Check if the chunk size is exceeded, if so, save the current chunk and start a new chunk
-        if accumulator + v > chunk_size:
-            chunks.append(obj_ids_in_chunk)
-            obj_ids_in_chunk = []
-            accumulator = 0
-
-        # Increase the accumulator and add the object id to the current chunk
-        accumulator += v
-        obj_ids_in_chunk.append(k)
-
-    # Add the last chunk if it is not empty
-    if obj_ids_in_chunk:
-        chunks.append(obj_ids_in_chunk)
-
-    return chunks
