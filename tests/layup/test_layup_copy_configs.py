074e7559bb5f93f3561e46ebdf138227b38f0eca
Wilson Beebe <wbeebe@uw.edu>
2025-05-15 11:18:48 -0700

    test commit

--- tests/layup/test_layup_copy_configs.py
+++ tests/layup/test_layup_copy_configs.py
@@ -1,7 +1,8 @@
 import os
+
 import pytest
 
-
+# Test simple comment
 def test_layupCopyConfigs(tmp_path):
     from layup.utilities.layup_copy_configs import copy_demo_configs
 
@@ -18,9 +19,7 @@ def test_layupCopyConfigs(tmp_path):
     with pytest.raises(SystemExit) as e:
         copy_demo_configs(dummy_folder, "all", False)
 
-    assert e.value.code == "ERROR: filepath {} supplied for filepath argument does not exist.".format(
-        dummy_folder
-    )
+    assert e.value.code == f"ERROR: filepath {dummy_folder} supplied for filepath argument does not exist."
 
     # test the error message if user supplies unrecognised keyword for which_configs variable
     with pytest.raises(SystemExit) as e2:
@@ -28,6 +27,7 @@ def test_layupCopyConfigs(tmp_path):
 
     assert e2.value.code == "String 'laphroaig' not recognised for 'configs' variable. Must be 'Default'."
 
+    # Test simple comment
     # test the error message if file exists and overwrite isn't forced
 
     with pytest.raises(SystemExit) as e3:
