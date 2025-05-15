519b4d0f6eec5eae26701be3aa321d8163256cbc
Wilson Beebe <wbeebe@uw.edu>
2025-05-15 10:06:05 -0700

    Cleanup test_predict

--- tests/layup/test_predict.py
+++ tests/layup/test_predict.py
@@ -10,17 +10,23 @@ from layup.utilities.file_io.CSVReader import CSVDataReader
 
 
 @pytest.mark.parametrize(
-    "chunk_size, time_step",
+    "chunk_size, time_step, input_format",
     [
-        (5, 0.5),
-        (5, 1),
-        (5, 2),
-        (3, 0.5),
-        (3, 1),
-        (3, 2),
+        (5, 0.5, "BCART_EQ"),
+        (5, 1, "BCART_EQ"),
+        (5, 2, "BCART_EQ"),
+        (3, 0.5, "BCART_EQ"),
+        (3, 1, "BCART_EQ"),
+        (3, 2, "BCART_EQ"),
+        (5, 0.5, "COM"),
+        (5, 1, "COM"),
+        (5, 2, "COM"),
+        (3, 0.5, "COM"),
+        (3, 1, "COM"),
+        (3, 2, "COM"),
     ],
 )
-def test_predict_cli(tmpdir, chunk_size, time_step):
+def test_predict_cli(tmpdir, chunk_size, time_step, input_format):
     """Test that the predict cli works for a small CSV file."""
     os.chdir(tmpdir)
 
@@ -34,35 +40,34 @@ def test_predict_cli(tmpdir, chunk_size, time_step):
             self.chunk = chunk_size
             self.station = "X05"
 
-    for test_file_name in ["predict_chunk_BCART_EQ.csv", "predict_chunk_COM.csv"]:
-        temp_out_file = f"test_output_{os.path.basename(test_file_name)}"
-        predict_cli(
-            cli_args=FakeCliArgs(),
-            input_file=get_test_filepath(test_file_name),
-            start_date=start,
-            end_date=end,
-            timestep_day=time_step,
-            output_file=temp_out_file,
-            cache_dir=None,
-        )
+    # The naming scheme for the test files indicates its orbit format
+    test_filename = f"predict_chunk_{input_format}.csv"
+    temp_out_file = f"test_output_{os.path.basename(test_filename)}"
+    predict_cli(
+        cli_args=FakeCliArgs(),
+        input_file=get_test_filepath(test_filename),
+        start_date=start,
+        end_date=end,
+        timestep_day=time_step,
+        output_file=temp_out_file,
+        cache_dir=None,
+    )
 
-        # Verify predict produced an output file
-        assert os.path.exists(temp_out_file)
-        # Create a new CSV reader to read in our output file
-        output_csv_reader = CSVDataReader(temp_out_file, "csv", primary_id_column_name="provID")
-        output_data = output_csv_reader.read_rows()
+    # Verify predict produced an output file
+    assert os.path.exists(temp_out_file)
+    # Create a new CSV reader to read in our output file
+    output_csv_reader = CSVDataReader(temp_out_file, "csv", primary_id_column_name="provID")
+    output_data = output_csv_reader.read_rows()
 
-        # Read the input data and get the provID column
-        input_csv_reader = CSVDataReader(
-            get_test_filepath(test_file_name), "csv", primary_id_column_name="provID"
-        )
-        input_data = input_csv_reader.read_rows()
+    # Read the input data and get the provID column
+    input_csv_reader = CSVDataReader(get_test_filepath(test_filename), "csv", primary_id_column_name="provID")
+    input_data = input_csv_reader.read_rows()
 
-        n_uniq_ids = sum([1 if id else 0 for id in set(input_data["provID"])])
-        number_of_predictions_per = len(np.arange(start, end + time_step, time_step))
+    n_uniq_ids = sum([1 if id else 0 for id in set(input_data["provID"])])
+    number_of_predictions_per = len(np.arange(start, end + time_step, time_step))
 
-        # ensure that have a prediction for each object at every time step
-        assert_equal(len(output_data), n_uniq_ids * number_of_predictions_per)
+    # ensure that have a prediction for each object at every time step
+    assert_equal(len(output_data), n_uniq_ids * number_of_predictions_per)
 
-        assert np.all(output_data["ra_deg"] <= 360.0) and np.all(output_data["ra_deg"] >= 0.0)
-        assert np.all(output_data["dec_deg"] <= 90.0) and np.all(output_data["dec_deg"] >= -90.0)
+    assert np.all(output_data["ra_deg"] <= 360.0) and np.all(output_data["ra_deg"] >= 0.0)
+    assert np.all(output_data["dec_deg"] <= 90.0) and np.all(output_data["dec_deg"] >= -90.0)
