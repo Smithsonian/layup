e577115ad4c4f1a1d06144cb2352b7bb6f372032
Ryan Lyttle <rlyttle09@qub.ac.uk>
2025-05-16 15:22:55 +0100

    unpack can now accept convert files
    updated unpack to accept convert files.
    for function parse_fit_results I've added a flag to be able to parse rows that dont have the orbit fit columns and their orbital parameters

--- src/layup/unpack.py
+++ src/layup/unpack.py
@@ -9,32 +9,92 @@ from layup.utilities.file_io.file_output import write_csv, write_hdf5
 logger = logging.getLogger(__name__)
 
 
+# Columns which may be added to the output data by the orbit fitting process
+ORBIT_FIT_COLS = [
+    ("csq", "f8"),  # Chi-square value
+    ("ndof", "i4"),  # Number of degrees of freedom
+    ("niter", "i4"),  # Number of iterations
+    ("method", "O"),  # Method used for orbit fitting
+    ("flag", "i4"),  # Single-character flag indicating success of the fit
+]
+
 INPUT_READERS = {
     "csv": CSVDataReader,
     "hdf5": HDF5DataReader,
 }
 
 
-def _get_result_dtypes(primary_id_column_name: str, state: list, sigma: list):
-    """Helper function to create the result dtype with the correct primary ID column name."""
-   
-    return np.dtype(
-        [
-            (primary_id_column_name, "O"),  # Object ID
-            ("csq", "f8"),  # Chi-square value
-            ("ndof", "i4"),  # Number of degrees of freedom
-            *[(state[i], "f8") for i in range(6)],  # State vector elements
-            *[(sigma[i], "f8") for i in range(6)],  # Sigma vector elements
-            ("epochMJD_TDB", "f8"),  # Epoch
-            ("niter", "i4"),  # Number of iterations
-            ("method", "O"),  # Method used for orbit fitting
-            ("flag", "i4"),  # Single-character flag indicating success of the fit
-            ("FORMAT", "O"),  # Orbit format
-        ]
-    )
+def _get_result_dtypes(primary_id_column_name: str, state: list, sigma: list, orbit_col_flag):
+    """
+    Creates a structured numpy dtype for the output data based on the provided parameters.
+
+    Parameters
+    ----------
+    primary_id_column_name : str
+        The name of the primary ID column (e.g., object identifier).
+    state : list
+        A list of parameter names corresponding to the state vector elements (e.g., position or orbital parameters).
+    sigma : list
+        A list of parameter names corresponding to the uncertainties (sigma) of the state vector elements.
+    orbit_col_flag : bool
+        A flag indicating whether orbit fitting columns (e.g., chi-square, degrees of freedom) should be included.
+
+    Returns
+    -------
+    np.dtype
+        A structured numpy dtype defining the format of the output array.
+    """
 
 
-def unpack(res, name, orbit_para, _RESULT_DTYPES):
+    if orbit_col_flag == True:
+        return np.dtype(
+            [
+                (primary_id_column_name, "O"),  # Object ID
+                ("csq", "f8"),  # Chi-square value
+                ("ndof", "i4"),  # Number of degrees of freedom
+                (state[0], "f8"),  # The first of 6 state vector elements
+                (sigma[0], "f8"),  # The first of 6 sigma vector elements
+                (state[1], "f8"),
+                (sigma[1], "f8"),
+                (state[2], "f8"),
+                (sigma[2], "f8"),
+                (state[3], "f8"),
+                (sigma[3], "f8"),
+                (state[4], "f8"),
+                (sigma[4], "f8"),
+                (state[5], "f8"),  # The last of 6 state vector elements
+                (sigma[5], "f8"),  # The last of 6 state vector elements
+                ("epochMJD_TDB", "f8"),  # Epoch
+                ("niter", "i4"),  # Number of iterations
+                ("method", "O"),  # Method used for orbit fitting
+                ("flag", "i4"),  # Single-character flag indicating success of the fit
+                ("FORMAT", "O"),  # Orbit format
+            ]
+        )
+
+    else:
+        return np.dtype(
+            [
+                (primary_id_column_name, "O"),  # Object ID
+                (state[0], "f8"),  # The first of 6 state vector elements
+                (sigma[0], "f8"),  # The first of 6 sigma vector elements
+                (state[1], "f8"),
+                (sigma[1], "f8"),
+                (state[2], "f8"),
+                (sigma[2], "f8"),
+                (state[3], "f8"),
+                (sigma[3], "f8"),
+                (state[4], "f8"),
+                (sigma[4], "f8"),
+                (state[5], "f8"),  # The last of 6 state vector elements
+                (sigma[5], "f8"),  # The last of 6 state vector elements
+                ("epochMJD_TDB", "f8"),  # Epoch
+                ("FORMAT", "O"),  # Orbit format
+            ]
+        )
+
+
+def unpack(res, name, orbit_para, _RESULT_DTYPES, orbit_cols_flag, format):
     """
     Unpacks a file containing a covariance matrix into associated uncertainties.
 
@@ -44,44 +104,58 @@ def unpack(res, name, orbit_para, _RESULT_DTYPES):
         The parsed fit result.
     name : str
         name of the object
-    orbit_para : list 
-        A list of parameter names corresponding to the state vector elements 
+    orbit_para : list
+        A list of parameter names corresponding to the state vector elements
     _RESULT_DTYPES : np,dtype
-        A structured numpy dtype defining the format of the output array. 
+        A structured numpy dtype defining the format of the output array.
     Returns
     -------
-    output : numpy array 
+    output : numpy array
         the numpy array containing the unpacked data.
+    orbit_cols_flag : bool
+        flag if input file has orbit fit columns
+    format : str
+        orbital format of data
     """
 
     j = [0, 7, 14, 21, 28, 35]  # location of cov_ii [00,11,22,33,44,55]
     error_list = []
     for i, orbit_para in enumerate(orbit_para):
         error_list.append(np.sqrt(res.cov[j[i]]))
-    state_error_pairs = [(res.state[i], error_list[i]) for i in range(6)]
-
-    # Flatten the state-error pairs into a single tuple
-    state_error_tuple = tuple(item for pair in state_error_pairs for item in pair)
-
-    # Construct the output array
-    output = np.array(
-        [
-            (
-                name,
-                res.csq,
-                res.ndof,
-            )
-            + state_error_tuple
-            + (
-                res.epoch - 2400000.5,  # changing from jd back to mjd
-                res.niter,
-                res.method,
-                res.flag,
-                "BCART",  # The base format returned by the C++ code
-            )
-        ],
-        dtype=_RESULT_DTYPES,
-    )
+    state_error_tuple = tuple(item for pair in zip(res.state, error_list) for item in pair)
+    if orbit_cols_flag == True:
+        # Construct the output array
+        output = np.array(
+            [
+                (
+                    name,
+                    res.csq,
+                    res.ndof,
+                )
+                + state_error_tuple
+                + (
+                    res.epoch - 2400000.5,  # changing from jd back to mjd
+                    res.niter,
+                    res.method,
+                    res.flag,
+                    format,  # The base format returned by the C++ code
+                )
+            ],
+            dtype=_RESULT_DTYPES,
+        )
+    else:
+        # Construct the output array
+        output = np.array(
+            [
+                (name,)
+                + state_error_tuple
+                + (
+                    res.epoch - 2400000.5,  # changing from jd back to mjd
+                    format,  # The base format returned by the C++ code
+                )
+            ],
+            dtype=_RESULT_DTYPES,
+        )
 
     return output
 
@@ -108,7 +182,7 @@ def unpack_cli(
         The format of the output file. Must be one of: "csv", "hdf5"
     chunk_size : int, optional (default=10_000)
         The number of rows to read in at a time.
-  
+
     cli_args : argparse, optional (default=None)
         The argparse object that was created when running from the CLI.
     """
@@ -134,6 +208,12 @@ def unpack_cli(
     )
 
     sample_data = sample_reader.read_rows(block_start=0, block_size=1)
+
+    orbit_fit_col_names = [col[0] for col in ORBIT_FIT_COLS]  # Extract column names from ORBIT_FIT_COLS
+    if all(col in sample_data.dtype.names for col in orbit_fit_col_names):
+        orbit_cols_flag = True
+    else:
+        orbit_cols_flag = False
 
     # Check orbit format in the file
     input_format = None
@@ -161,7 +241,9 @@ def unpack_cli(
             "sigma_t_p_MJD_TDB",
         ]
 
-    _RESULT_DTYPES = _get_result_dtypes(primary_id_column_name, orbit_para, orbit_para_sigma)
+    _RESULT_DTYPES = _get_result_dtypes(
+        primary_id_column_name, orbit_para, orbit_para_sigma, orbit_col_flag=orbit_cols_flag
+    )
 
     # read data
     full_reader = reader_class(
@@ -184,9 +266,9 @@ def unpack_cli(
         for row in chunk_data:
 
             res = parse_fit_result(
-                row
+                row, orbit_cols_flag, orbit_para
             )  # res.epoch is converted to jd in this function. It is converted back to mjd in output
-            res_unpacked = unpack(res, row[0], orbit_para, _RESULT_DTYPES)
+            res_unpacked = unpack(res, row[0], orbit_para, _RESULT_DTYPES, orbit_cols_flag, format)
 
             # All results go to a single output file
             if file_format == "hdf5":
